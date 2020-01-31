64babd0a07eaab905314282c85baa497b181a916
Clément Puybareau <clement.p49@gmail.com>
2020-01-31 16:55:12 +0100

    Changement de la machine à état de pilot.c avec les pointeurs de fonctions

--- version_a_completer/src/mrRobot/pilot.c
+++ version_a_completer/src/mrRobot/pilot.c
@@ -7,8 +7,57 @@
 #define DEFAULT_SPEED 0
 
 
-typedef enum {DEFAULT_EVENT=0, SET_VEL, CHECK} Event;
-typedef enum {INIT=0, IDLE, RUNNING} State;
+typedef enum {SET_VEL=0, CHECK=1} Event;
+typedef enum {IDLE=0, RUNNING=1} State;
+
+/**
+typedef void (*f_ptr_generic)(void);
+typedef void (*f_ptr_vel)(VelocityVector vel);
+typedef void (*f_ptr_ev_vel)(Event event, VelocityVector vel);
+
+typedef struct {
+    f_ptr_ev_vel state_f;
+    f_ptr_vel set_vel_f;
+    f_ptr_generic check_vel;
+} State_s;
+
+static void init_f();
+static void idle_f(Event event, VelocityVector vel);
+static void idle_vel_f(VelocityVector vel);
+static void running_f(Event event, VelocityVector vel);
+static void running_vel_f(VelocityVector vel);
+static void running_check_f();
+
+static const State_s INIT_s = {
+    .state_f = &init_f
+};
+static const State_s IDLE_s = {
+    .state_f = idle_f,
+    .set_vel_f = &idle_vel_f
+};
+static const State_s RUNNING_s = {
+    .state_f = running_f,
+    .set_vel_f = running_vel_f,
+    .check_vel = running_check_f
+};
+
+static State_s etats[] = {INIT_s, IDLE_s, RUNNING_s};
+*/
+
+static void idle_f_vel(VelocityVector vel);
+static void idle_f_check(VelocityVector vel);
+static void running_f_vel(VelocityVector vel);
+static void running_f_check(VelocityVector vel);
+
+typedef void (*f_ptr)(VelocityVector vel);
+
+static f_ptr state_f_list[] =
+    {
+        &idle_f_vel,       // 0 = 00 = IDLE | SET_VEL
+        &idle_f_check,     // 1 = 01 = IDLE | CHECK
+        &running_f_vel,    // 2 = 10 = RUNNING | SET_VEL
+        &running_f_check   // 3 = 11 = RUNNING | CHECK
+    };
 
 static const VelocityVector DEFAULT_VELOCITY_VECTOR = {
     .dir = STOP,
@@ -53,15 +102,21 @@ static void send_mvt(VelocityVector vel) {
     Robot_setWheelsVelocity(vel_r, vel_l);
 }
 
+
+/**
+ * Fonction run permettant la mise à jour de l'état du pilote
+ * Machine à état asynchrone
+ */
+static void run(Event event, VelocityVector vel) {
+    state_f_list[current_state<<1|event](vel);
+}
+
 /**
  * Met à jour l'état actuel du pilote et effectue
  * les actions en entrée de cet état
  */
 static void switch_state(State new_state) {
     switch (new_state) {
-        case INIT:
-            current_state = INIT;
-            break;
         case IDLE:
             send_mvt(DEFAULT_VELOCITY_VECTOR);
             current_state = IDLE;
@@ -74,47 +129,29 @@ static void switch_state(State new_state) {
 }
 
 /**
- * Fonction run permettant la mise à jour de l'état du pilote
- * Machine à état asynchrone
+ * Fonctions de l'état IDLE
  */
-static void run(Event event, VelocityVector vel) {
-    switch (current_state) {
-        // INIT : passe à l'état IDLE directement
-        case INIT:
-            switch_state(IDLE);
-            break;
-        // IDLE : ne fait quelque chose que si l'event est un SET_VEL
-        case IDLE:
-            switch (event) {
-                case SET_VEL:
-                    if (vel.power != 0) {
-                        send_mvt(vel);
-                        switch_state(RUNNING);
-                    }
-                    break;
-                case CHECK: default: break;
-            }
-            break;
-        // RUNNING : revient à IDLE si vitesse à 0
-        // Ne check pas s'il y a eu un bump !
-        case RUNNING:
-            switch (event) {
-                case SET_VEL:
-                    if (vel.power == 0) {
-                        switch_state(IDLE);
-                    } else {
-                        send_mvt(vel);
-                    }
-                    break;
-                case CHECK:
-                    if (has_bumped()) {
-                        switch_state(IDLE);
-                    }
-                    break;
-                default: break;
-            }
-            break;
-        default: break;
+static void idle_f_vel(VelocityVector vel) {
+    if (vel.power != 0) {
+        send_mvt(vel);
+        switch_state(RUNNING);
+    }
+}
+static void idle_f_check(VelocityVector vel) {}
+
+/**
+ * Fonctions de l'état RUNNING
+ */
+static void running_f_vel(VelocityVector vel) {
+    if (vel.power == 0) {
+        switch_state(IDLE);
+    } else {
+        send_mvt(vel);
+    }
+}
+static void running_f_check(VelocityVector vel) {
+    if (has_bumped()) {
+        switch_state(IDLE);
     }
 }
 
@@ -125,7 +162,7 @@ static void run(Event event, VelocityVector vel) {
 extern void Pilot_start() {
     Pilot_new();
     Robot_start();
-    run(DEFAULT_EVENT, DEFAULT_VELOCITY_VECTOR);
+    switch_state(IDLE);
 }
 
 /**
