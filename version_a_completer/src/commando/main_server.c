7957262a3b62532f170060f8e9c5a602cc2be744
Clément Puybareau <clement.p49@gmail.com>
2020-02-08 13:59:25 +0100

    CMake c'est de la merde

--- version_a_completer/src/commando/main_server.c
+++ version_a_completer/src/commando/main_server.c
@@ -2,7 +2,7 @@
 // Created by cleme on 05/02/2020.
 //
 
-#include "telco/client.h"
+#include "server.h"
 
 
 /**
@@ -10,7 +10,7 @@
  */
 int main (int argc, char *argv[])
 {
-    Client_start();
-    Client_stop();
+    Server_start();
+    Server_stop();
     return 0;
 }
--- version_a_completer/src/telco/main_client.c
+++ version_a_completer/src/telco/main_client.c
@@ -2,7 +2,7 @@
 // Created by cleme on 05/02/2020.
 //
 
-#include "commando/server.h"
+#include "client.h"
 
 
 /**
@@ -10,7 +10,7 @@
  */
 int main (int argc, char *argv[])
 {
-    Server_start();
-    Server_stop();
+    Client_start();
+    Client_stop();
     return 0;
 }
