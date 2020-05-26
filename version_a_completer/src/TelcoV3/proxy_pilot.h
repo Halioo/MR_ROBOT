6d647ac52ca2b79d405dc82a578f8ad2e5bef9bb
Gwendal <gwendal.couedel@reseau.eseo.fr>
2020-05-26 12:03:40 +0200

    implémentation de proxy_pilot, il va surement falloir modifier la gestion de la socket à l'avenir

--- version_a_completer/src/TelcoV3/proxy_pilot.h
+++ version_a_completer/src/TelcoV3/proxy_pilot.h
@@ -5,9 +5,11 @@
 #ifndef PROXY_PILOT_H
 #define PROXY_PILOT_H
 
-#include "../../lib/include/util.h"
+#include "util.h"
+#include <robocom.h>
 
 extern void Pilot_toggleES();
-extern void Pilot_SetVelocity(VelocityVector);
+extern void Pilot_SetVelocity(VelocityVector vel);
+extern void Proxy_Pilot_store_Socket(int socket);
 
 #endif //PROXY_PILOT_H
